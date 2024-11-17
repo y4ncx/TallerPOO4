b121b0ddb48c7e9e2b229f350c03b39d10858640
y4ncx <y4ncxs@gmail.com>
2024-11-17 17:19:00 -0500

    Update PruebaCuentaBancaria.java

--- src/PruebaCuentaBancaria.java
+++ src/PruebaCuentaBancaria.java
@@ -1,5 +1,6 @@
 public class PruebaCuentaBancaria {
     public static void main(String[] args) {
         CuentaBancaria cuenta = new CuentaBancaria(1000.55);
+        //System.out.println(El saldo inicial es de = " + cuenta.saldo);
     }
 }
